7d974a5d52cec389ffd2f6308c60895cdb16e6f4
jorgenwh <jorgenwhen@gmail.com>
2023-12-29 16:08:43 +0100

    data

--- train.py
+++ train.py
@@ -4,5 +4,5 @@ import torch
 from helpers import read_text, preprocess_text
 
 
-
-data, vocab_size, char_to_index, index_to_char = preprocess_text(read_text("data/shakespeare.txt"))
+data, vocab_size, char_to_index, index_to_char = preprocess_text(read_text("data/tinyshakespeare.txt"))
+assert data.dtype == torch.int64
